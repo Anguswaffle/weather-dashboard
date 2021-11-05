2de2800f0583f760781956d09eb67dfedf7717ca
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-11-04 23:34:46 -0700

    Added forecast and button history

---
+++ assets/README.md
@@ -0,0 +1,11 @@
+# 06 - Weather Dashboard
+
+https://anguswaffle.github.io/weather-dashboard/
+
+## Functionality
+
+
+## Mock-Up
+The following image shows the web application's appearance and functionality:
+
+![Scheduler Preview](./assets/images/preview.jpg)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var inputFieldEl = document.querySelector('#city-input')
 var submitBtn = document.querySelector('#submit-btn')
+var forecastContainerEl = document.querySelector('#forecast')
+var currentCityEl = document.querySelector('#current-city');
 
 
 // Initiates fetch requests and populateData function
-function fetchData(event) {
-    event.preventDefault();
+function fetchData(){
     var cityName = inputFieldEl.value;
     var apiKey = '162c997af48d86f877c2812ef39f3538';
     var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
@@ -12,7 +13,11 @@ function fetchData(event) {
     // Fetch to grab city coordinates
     fetch(requestUrl)
         .then(response => {
-            return response.json();
+            if (response.status >= 200 && response.status <= 299) {
+                return response.json();
+            } else {
+                throw Error(response.statusText);
+            }
         })
         .then(weatherData => {
             // console.log(weatherData);
@@ -28,25 +33,24 @@ function fetchData(event) {
                 .then(forecastData => {
                     // Both data objects are passed into populateData
                     populateData(weatherData, forecastData);
+                    saveCity();
                 })
-            // .catch(err => {
-            //     errorCatch(err);
-            // })
 
         })
-        // .catch(err => {
-        //     document.querySelector('#current-city').textContent = err.message;
-        // });
-        
+        .catch((error) => {
+            currentCityEl.textContent = error;
+        });
+
 }
 
 function populateData(currentData, forecastData) {
-    console.log(currentData);
-    console.log(forecastData);
+
+    // Removes previous 5-day forecast, if any
+    removeForecastEls();
 
     // Populates current city info first
     // HTML elements for current city
-    var currentCityEl = document.querySelector('#current-city');
+
     var currentTempEl = document.querySelector('#current-temp')
     var currentWindEl = document.querySelector('#current-wind')
     var currentHumidityEl = document.querySelector('#current-humidity')
@@ -59,7 +63,6 @@ function populateData(currentData, forecastData) {
     var currentWind = convertWind(currentData.wind.deg, currentData.wind.speed);
     var currentHumidity = currentData.main.humidity + '%';
     var currentUV = forecastData.current.uvi;
-    console.log(forecastData.current.uvi);
 
     // Sets text
     currentCityEl.textContent = currentData.name + ' (' + cityDate + ') '
@@ -75,13 +78,45 @@ function populateData(currentData, forecastData) {
 
     // Sets background color for UV Index integer
     checkUV(currentUV);
-    
-    
+
+    // Forecast stuff
+    for (var i = 1; i <= 5; i++) {
+        var cardDivEl = document.createElement('div')
+        cardDivEl.setAttribute('class', 'card');
+        var dateEl = document.createElement('h2');
+        var iconEl = document.createElement('img');
+        var tempEl = document.createElement('p');
+        var windEl = document.createElement('p');
+        var humidityEl = document.createElement('p');
+
+        dateEl.textContent = moment(cityDate).add(i, 'day').format('L');
+        var icon = 'http://openweathermap.org/img/wn/' + forecastData.daily[i].weather[0].icon + '.png';
+        iconEl.setAttribute('src', icon);
+        tempEl.textContent = forecastData.daily[i].temp.day + '°C';
+        var windDeg = forecastData.daily[i].wind_deg;
+        var windSpd = forecastData.daily[i].wind_speed;
+        windEl.textContent = convertWind(windDeg, windSpd);
+        humidityEl.textContent = forecastData.daily[i].humidity + '%'
+
+        forecastContainerEl.append(cardDivEl);
+        cardDivEl.append(dateEl);
+        cardDivEl.append(iconEl);
+        cardDivEl.append(tempEl);
+        cardDivEl.append(windEl);
+        cardDivEl.append(humidityEl);
+    }
 
 }
 
+// Removes previous forecast card elements
+function removeForecastEls() {
+    for (var i = 0; forecastContainerEl.children.length > i; i) {
+        forecastContainerEl.removeChild(forecastContainerEl.childNodes[i])
+    }
+}
+
 // Checks uv index int then adds a corresponding class to uv element
-function checkUV(uvi){
+function checkUV(uvi) {
     var quality = ''
     if (uvi < 3) quality = 'good'
     if (uvi > 3 && uvi < 8) quality = 'moderate'
@@ -91,36 +126,41 @@ function checkUV(uvi){
 }
 
 // Returns wind direction and speed using cardinal directions
-function convertWind(deg, speed){
+function convertWind(deg, speed) {
 
-    console.log(deg + speed);
-    if(deg >= 349 || deg < 11 ) deg = "N"
-    if(deg >= 11 && deg < 34) deg = "NNE";
-    if(deg >= 34 && deg < 56) deg = "NE";
-    if(deg >= 56 && deg < 79) deg = "ENE";
-    if(deg >= 79 && deg < 101) deg = "E";
-    if(deg >= 101 && deg < 124) deg = "ESE";
-    if(deg >= 124 && deg < 146) deg = "SE";
-    if(deg >= 146 && deg < 169) deg = "SSE";
-    if(deg >= 169 && deg < 191) deg = "S";
-    if(deg >= 191 && deg < 214) deg = "SSW";
-    if(deg >= 214 && deg < 236) deg = "SW";
-    if(deg >= 236 && deg < 259) deg = "WSW";
-    if(deg >= 259 && deg < 281) deg = "W";
-    if(deg >= 281 && deg < 304) deg = "WNW";
-    if(deg >= 304 && deg < 326) deg = "NW";
-    if(deg >= 326 && deg < 349) deg = "NNW";
+    if (deg >= 349 || deg < 11) deg = "N"
+    if (deg >= 11 && deg < 34) deg = "NNE";
+    if (deg >= 34 && deg < 56) deg = "NE";
+    if (deg >= 56 && deg < 79) deg = "ENE";
+    if (deg >= 79 && deg < 101) deg = "E";
+    if (deg >= 101 && deg < 124) deg = "ESE";
+    if (deg >= 124 && deg < 146) deg = "SE";
+    if (deg >= 146 && deg < 169) deg = "SSE";
+    if (deg >= 169 && deg < 191) deg = "S";
+    if (deg >= 191 && deg < 214) deg = "SSW";
+    if (deg >= 214 && deg < 236) deg = "SW";
+    if (deg >= 236 && deg < 259) deg = "WSW";
+    if (deg >= 259 && deg < 281) deg = "W";
+    if (deg >= 281 && deg < 304) deg = "WNW";
+    if (deg >= 304 && deg < 326) deg = "NW";
+    if (deg >= 326 && deg < 349) deg = "NNW";
 
     return speed + ' km/h - ' + deg;
 }
 
 function resetPage() {
     inputFieldEl.value = '';
+    removeForecastEls();
 }
 
-// To be written later
-// function errorCatch(error){
+function saveCity() {
+    var newBtn = document.createElement('button')
+    newBtn.setAttribute('class', 'history-btn')
+    newBtn.textContent = inputFieldEl.value;
+    document.querySelector('#past-searches').append(newBtn);
+}
 
-// }
-
-submitBtn.addEventListener('click', fetchData);
+submitBtn.addEventListener('click', function(event){
+    event.preventDefault;
+    fetchData();
+});
